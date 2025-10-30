f3612f876c4428a0124c6db190397f7c7f1da5c1
wiktor <wiktor.kycia@gmail.com>
2025-10-30 18:44:30 +0100

    add some css and craete a first route

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
-import express, { Express } from 'express'
+import express, { Express, Request, Response } from 'express'
+import path from 'path'
 
 const app: Express = express()
+
+app.use('/static', express.static(path.join(__dirname, '/static')))
+
+app.get('/', (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, 'static', 'index.html'))
+})
 
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
