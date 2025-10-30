62ddb3aa7763acc068a20409936de6bad9098f1e
wiktor <wiktor.kycia@gmail.com>
2025-10-30 18:27:53 +0100

    modify the prettier format settings

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import express, { Express } from 'express'
 const app: Express = express()
 
 app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
+    console.log('App is running on http://localhost:3000')
 })
