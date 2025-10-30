696d67e36bcd68185fd97147637ce93fa1ec88c8
wiktor <wiktor.kycia@gmail.com>
2025-10-30 18:57:45 +0100

    add middleware

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, NextFunction, Request, Response } from 'express'
 import path from 'path'
 
 const app: Express = express()
 
+// middleware:
 app.use('/static', express.static(path.join(__dirname, '..', '/static')))
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(req.path)
+    next()
+})
 
 app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '..', 'static', 'index.html'))
