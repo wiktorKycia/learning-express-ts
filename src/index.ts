72d08e01fecf1b909b29dea07ea3a0c1df7d2fe3
wiktor <wiktor.kycia@gmail.com>
2025-10-30 19:08:03 +0100

    implement a form in a pawcio way

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,21 @@ app.get('/', (req: Request, res: Response) => {
     res.sendFile(path.join(__dirname, '..', 'static', 'index.html'))
 })
 
+app.post('/kontakt', (req: Request, res: Response) => {
+    const body: Buffer[] = []
+    req.on('data', (chunk) => {body.push(chunk)})
+
+    req.on('end', async () => {
+        const parsedBody = Buffer.concat(body).toString()
+        const message = parsedBody.split('=')[1]
+        console.log(message)
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        res.end()
+    })
+
+})
+
 app.listen(3000, () => {
     console.log('App is running on http://localhost:3000')
 })
